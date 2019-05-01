abb7675e0d1e83ae7e3ec27f97a52f05bdda129d
George MacRorie <me@georgemac.com>
2019-05-01 21:39:35 +0100

    Initial cmd/rate binary implementation

--- cmd/rate/main.go
+++ cmd/rate/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/georgemac/rate/pkg/rate"
+	"github.com/georgemac/rate/pkg/sync"
+)
+
+func printHelp() {
+	fmt.Println("rate [flags] <proxied_url>")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
-	fmt.Println("rate")
+	var (
+		port = flag.String("port", "4040", "port on which to service rate limiter")
+		rpm  = flag.Int("rpm", 100, "requests per minute")
+	)
+
+	flag.Parse()
+
+	target := flag.Arg(0)
+	if target == "" {
+		printHelp()
+		os.Exit(1)
+	}
+
+	url, err := url.Parse(target)
+	checkError(err)
+
+	var (
+		proxy          = httputil.NewSingleHostReverseProxy(url)
+		acquirer, aerr = sync.NewKeyedSemaphore(*rpm, time.Minute)
+		waiterOption   = rate.WithWaiter(rate.NextIntervalWaiter(time.Minute))
+		limiter        = rate.NewLimiter(proxy, acquirer, waiterOption)
+	)
+
+	checkError(aerr)
+
+	http.ListenAndServe(":"+*port, limiter)
 }
